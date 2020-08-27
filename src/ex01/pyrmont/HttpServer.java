4fcf3d7e6fdb903e8e590029749fe51923300d0a
lmw <pinkpig_zsy@163.com>
2020-08-28 07:52:05 +0800

    第一章

--- src/ex01/pyrmont/HttpServer.java
+++ src/ex01/pyrmont/HttpServer.java
@@ -10,66 +10,51 @@ import java.io.File;
 
 public class HttpServer {
 
-  /** WEB_ROOT is the directory where our HTML and other files reside.
-   *  For this package, WEB_ROOT is the "webroot" directory under the working
-   *  directory.
-   *  The working directory is the location in the file system
-   *  from where the java command was invoked.
-   */
-  public static final String WEB_ROOT =
-    System.getProperty("user.dir") + File.separator  + "webroot";
+    /**
+     * WEB_ROOT is the directory where our HTML and other files reside.
+     * For this package, WEB_ROOT is the "webroot" directory under the working
+     * directory.
+     * The working directory is the location in the file system
+     * from where the java command was invoked.
+     */
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
 
-  // shutdown command
-  private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+    // shutdown command
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
 
-  // the shutdown command received
-  private boolean shutdown = false;
+    // the shutdown command received
+    private boolean shutdown = false;
 
-  public static void main(String[] args) {
-    HttpServer server = new HttpServer();
-    server.await();
-  }
-
-  public void await() {
-    ServerSocket serverSocket = null;
-    int port = 8080;
-    try {
-      serverSocket =  new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-      System.exit(1);
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        server.await();
     }
 
-    // Loop waiting for a request
-    while (!shutdown) {
-      Socket socket = null;
-      InputStream input = null;
-      OutputStream output = null;
-      try {
-        socket = serverSocket.accept();
-        input = socket.getInputStream();
-        output = socket.getOutputStream();
+    private void await() {
+        // Loop waiting for a request
+        while (!shutdown) {
+            try (ServerSocket serverSocket = new ServerSocket(8080, 1, InetAddress.getByName("127.0.0.1"));
+                 Socket socket = serverSocket.accept();
+                 InputStream input = socket.getInputStream();
+                 OutputStream output = socket.getOutputStream()
+            ) {
+                // create Request object and parse
+                Request request = new Request(input);
+                request.parse();
 
-        // create Request object and parse
-        Request request = new Request(input);
-        request.parse();
+                // create Response object
+                Response response = new Response(output);
+                response.setRequest(request);
+                response.sendStaticResource();
 
-        // create Response object
-        Response response = new Response(output);
-        response.setRequest(request);
-        response.sendStaticResource();
+                // Close the socket
+                socket.close();
 
-        // Close the socket
-        socket.close();
-
-        //check if the previous URI is a shutdown command
-        shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
-      }
-      catch (Exception e) {
-        e.printStackTrace();
-        continue;
-      }
+                //check if the previous URI is a shutdown command
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-  }
 }
