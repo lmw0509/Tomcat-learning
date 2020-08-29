67fcbd424109686690b98ea0b7dfa98244ef8c0b
lmw <pinkpig_zsy@163.com>
2020-08-29 22:14:16 +0800

    手写连接器

--- src/ex03/pyrmont/connector/http/HttpConnector.java
+++ src/ex03/pyrmont/connector/http/HttpConnector.java
@@ -6,41 +6,35 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpConnector implements Runnable {
-
-  boolean stopped;
-  private String scheme = "http";
-
-  public String getScheme() {
-    return scheme;
-  }
-
-  public void run() {
-    ServerSocket serverSocket = null;
-    int port = 8080;
-    try {
-      serverSocket =  new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+    public String getScheme() {
+        return "http";
     }
-    catch (IOException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    while (!stopped) {
-      // Accept the next incoming connection from the server socket
-      Socket socket = null;
-      try {
-        socket = serverSocket.accept();
-      }
-      catch (Exception e) {
-        continue;
-      }
-      // Hand this socket off to an HttpProcessor
-      HttpProcessor processor = new HttpProcessor(this);
-      processor.process(socket);
-    }
-  }
 
-  public void start() {
-    Thread thread = new Thread(this);
-    thread.start();
-  }
+    public void run() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(8080, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (true) {
+            // Accept the next incoming connection from the server socket
+            Socket socket;
+            try {
+                socket = serverSocket.accept();
+            } catch (Exception e) {
+                continue;
+            }
+
+            // Hand this socket off to an HttpProcessor
+            HttpProcessor processor = new HttpProcessor(this);
+            processor.process(socket);
+        }
+    }
+
+    public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
+    }
 }
