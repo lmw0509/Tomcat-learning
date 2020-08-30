09638144cb31df7ab5240899067ff1309b162da3
lmw <pinkpig_zsy@163.com>
2020-08-31 07:29:50 +0800

    第五章

--- src/ex05/pyrmont/startup/Bootstrap2.java
+++ src/ex05/pyrmont/startup/Bootstrap2.java
@@ -15,43 +15,42 @@ import org.apache.catalina.Wrapper;
 import org.apache.catalina.connector.http.HttpConnector;
 
 public final class Bootstrap2 {
-  public static void main(String[] args) {
-    HttpConnector connector = new HttpConnector();
-    Wrapper wrapper1 = new SimpleWrapper();
-    wrapper1.setName("Primitive");
-    wrapper1.setServletClass("PrimitiveServlet");
-    Wrapper wrapper2 = new SimpleWrapper();
-    wrapper2.setName("Modern");
-    wrapper2.setServletClass("ModernServlet");
+    public static void main(String[] args) {
+        HttpConnector connector = new HttpConnector();
+        Wrapper wrapper1 = new SimpleWrapper();
+        wrapper1.setName("Primitive");
+        wrapper1.setServletClass("PrimitiveServlet");
+        Wrapper wrapper2 = new SimpleWrapper();
+        wrapper2.setName("Modern");
+        wrapper2.setServletClass("ModernServlet");
 
-    Context context = new SimpleContext();
-    context.addChild(wrapper1);
-    context.addChild(wrapper2);
+        Context context = new SimpleContext();
+        context.addChild(wrapper1);
+        context.addChild(wrapper2);
 
-    Valve valve1 = new HeaderLoggerValve();
-    Valve valve2 = new ClientIPLoggerValve();
+        Valve valve1 = new HeaderLoggerValve();
+        Valve valve2 = new ClientIPLoggerValve();
 
-    ((Pipeline) context).addValve(valve1);
-    ((Pipeline) context).addValve(valve2);
+        ((Pipeline) context).addValve(valve1);
+        ((Pipeline) context).addValve(valve2);
 
-    Mapper mapper = new SimpleContextMapper();
-    mapper.setProtocol("http");
-    context.addMapper(mapper);
-    Loader loader = new SimpleLoader();
-    context.setLoader(loader);
-    // context.addServletMapping(pattern, name);
-    context.addServletMapping("/Primitive", "Primitive");
-    context.addServletMapping("/Modern", "Modern");
-    connector.setContainer(context);
-    try {
-      connector.initialize();
-      connector.start();
+        Mapper mapper = new SimpleContextMapper();
+        mapper.setProtocol("http");
+        context.addMapper(mapper);
+        Loader loader = new SimpleLoader();
+        context.setLoader(loader);
+        // context.addServletMapping(pattern, name);
+        context.addServletMapping("/Primitive", "Primitive");
+        context.addServletMapping("/Modern", "Modern");
+        connector.setContainer(context);
+        try {
+            connector.initialize();
+            connector.start();
 
-      // make the application wait until we press a key.
-      System.in.read();
+            // make the application wait until we press a key.
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 }
